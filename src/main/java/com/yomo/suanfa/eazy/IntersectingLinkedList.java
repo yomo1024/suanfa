1ec37e351346eb4069ec1a80fec47e97503efd73
yomo <2686944979@qq.com>
2023-01-07 13:04:05 +0800

    相交链表

---
+++ src/main/java/com/yomo/suanfa/eazy/IntersectingLinkedList.java
@@ -0,0 +1,21 @@
+package com.yomo.suanfa.eazy;
+
+import com.yomo.suanfa.node.ListNode;
+
+/**
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+ *
+ * @author yomo
+ * @since 2023/1/7
+ */
+public class IntersectingLinkedList {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+}
